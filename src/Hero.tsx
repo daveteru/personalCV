598cbe1876bff8e9934246f3e57631a1c07f8ebc
Teru <218044131+daveteru@users.noreply.github.com>
2026-03-07 11:44:51 +0700

    fix : installed hero ui , add provider

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
     <div className="h-screen flex justify-end items-end mb-20 pb-9 text-white flex-col">
         <hr></hr>
         <span className="z-11">DESIGN + TECH</span>
-      <div className="relative z-10 w-100 h-140 bg-gray-400 pointer-events-none overflow-hidden "><img src="../asset/teru_profpic_2_bw.png" alt=""/> </div>
+      <div className="relative z-10 w-100 h-140 bg-gray-400 pointer-events-none overflow-hidden "><img src="/asset/teru_profpic_2_bw.png"/> </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router";
+import { HeroUIProvider } from "@heroui/react";
 import "./index.css";
 import Home from "./Home";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <HeroUIProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </HeroUIProvider>
     </HashRouter>
-  </StrictMode>
+  </StrictMode>,
 );
